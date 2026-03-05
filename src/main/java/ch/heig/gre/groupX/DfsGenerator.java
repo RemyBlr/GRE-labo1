971ff9e0d6d96852bdf1568bca025afa964fe3e8
RemyBlr <remy.bleuer@hotmail.com>
2026-03-05 08:44:07 +0100

    Added files

---
+++ src/main/java/ch/heig/gre/groupX/DfsGenerator.java
@@ -0,0 +1,13 @@
+package ch.heig.gre.groupX;
+
+import ch.heig.gre.maze.MazeBuilder;
+import ch.heig.gre.maze.MazeGenerator;
+
+// TODO : classe à compléter et documenter
+public final class DfsGenerator implements MazeGenerator {
+  @Override
+  public void generate(MazeBuilder builder, int from) {
+    // Mise à jour de l'interface graphique :
+    // builder.progressions().setLabel(..., ...);
+  }
+}
